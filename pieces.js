108ae799f0f8d18c582cdb4d6078e403d4336d11
Jérôme AUGUSTE <jerome_auguste@outlook.fr>
2023-03-10 16:24:50 +0100

    Added description and availability to the fiches

---
+++ pieces.js
@@ -0,0 +1,27 @@
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch('pieces-autos.json');
+const pieces = await reponse.json();
+
+// Création des balises 
+const article = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "(aucune description)";
+const availableElement = document.createElement("p");
+availableElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+
+//Rattachement de nos balises au DOM
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(availableElement);
